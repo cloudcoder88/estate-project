f8100f011e97087fb2338f25177b74bced06200b
cloudcoder88 <btech4104@gmail.com>
2025-08-04 00:03:27 +0100

    i moved plain html to angular components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,70 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import  {CommonModule} from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone:true,
 })
 export class AppComponent {
   title = 'real-estate-app';
+  properties=[
+    {name:'ikeja Apartment',
+    price:'120,000,000',
+    location:'lekki',
+    beds: 4,
+    baths: 4,
+    description: 'A luxurious apartment in the heart of Ikeja.',
+    image:'https://pictures-nigeria.jijistatic.net/176111110_MTIwMC0xNjAwLTNhOGU5YmQ2ZmY.webp',
+    
+    },
+    {name:'5bdrm Bungalow in goshen estate,Abuja',
+    price:'120,000,000',
+    location:'goshen estate,Abuja',
+    beds: 5,
+    baths: 6,
+    description: 'fully furnished,located in a serene environment, reliable power supply with backup generator,constant water supply,parking space',
+    image:'https://pictures-nigeria.jijistatic.net/175019180_MTQ4OC0xNjAwLTgyMGRmMzRhOTk.webp',
+
+    },
+    {
+      name:'Ajah Bungalow',
+      price:'65,000,000',
+      location:'Ajah, Lagos',
+      beds: 2,
+      baths: 2,
+      description: 'A cozy bungalow located in the peaceful Ajah area.',
+      image:'https://pictures-nigeria.jijistatic.net/164240528_MTYwMC0yMTMzLWRhMWQ4NmE3ZjM.webp',
+    },
+    {name:'Ikeja Lodge',
+    price:'1,000,000',
+    location:'Ikeja, Lagos',
+    beds: 1,
+    baths: 1,
+    description: 'A luxurious lodge with modern amenities in Ikeja.',
+    image:'https://cf.bstatic.com/xdata/images/hotel/max1024x768/645642762.jpg?k=46bec7c1caa1d746c339f5a1171247a427e636b97edb5e2fd9fbcd62067c4118&o',
+    },
+    {name:'Ikeja Lodge',
+    price:'1,000,000',
+    location:'Ikeja, Lagos',
+    beds: 1,
+    baths: 1,
+    description: 'A luxurious lodge with modern amenities in Ikeja.',
+    image:'https://cf.bstatic.com/xdata/images/hotel/max1024x768/645642762.jpg?k=46bec7c1caa1d746c339f5a1171247a427e636b97edb5e2fd9fbcd62067c4118&o',
+    },
+    {name:'Lekki Apartment',
+    price:'120,000,000',
+    location:'Lekki, Lagos',
+    beds: 4,
+    baths: 4,
+    description: 'A luxurious apartment in the heart of Lekki.',
+    image:'https://pictures-nigeria.jijistatic.net/176111110_MTIwMC0xNjAwLTNhOGU5YmQ2ZmY.webp',
+    }
+  ];
+  inquire(propertyName: string): void {
+    alert(`thanks for your interest in : ${propertyName}. we will get back to you shortly`);
+  }
 }
+
