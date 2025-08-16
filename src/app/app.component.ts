3ac8050aced598e70571ec7dd757c619ab32d5c2
cloudcoder88 <btech4104@gmail.com>
2025-08-17 00:28:13 +0100

    i added router-links for the nav bar buttons,i imported routerlinks in the app.components.file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink } from '@angular/router';
 import  {CommonModule} from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone:true,
