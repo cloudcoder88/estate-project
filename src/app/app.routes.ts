d9faa081efad9304430a4496aef395b2c630360b
cloudcoder88 <btech4104@gmail.com>
2025-08-16 23:36:29 +0100

    i created a contact component and added the components to path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
+ { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: '', component: HomeComponent },
-  { path: '**', redirectTo: '' }
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' } 
 ];
