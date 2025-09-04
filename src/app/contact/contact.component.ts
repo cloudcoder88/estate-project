0b0527cf3e43567634aae64cf4cd8ae5eef462fa
cloudcoder88 <btech4104@gmail.com>
2025-09-04 22:50:33 +0100

    i configured the contact.us page to provide backend routing

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-contact',
@@ -7,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+ contact={
+  fullName:'',
+  email:'',
+  message:'',
+  phone:''
+ }
 
+constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    this.http.post('http://localhost:5000/contact', this.contact).subscribe({
+      next: (res) => {
+        alert('✅ Message sent successfully!');
+        this.contact = { fullName: '', email: '', phone: '', message: '' }; // reset form
+      },
+      error: (err) => {
+        console.error('❌ Error sending message:', err);
+        alert('Something went wrong. Please try again.');
+      }
+    });
+  }
 }
